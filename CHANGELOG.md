c5dcf222753e3013925ddcaf3ecc0b3f71f71f14
Hector Castro <hectcastro@gmail.com>
2012-08-07 20:39:55 -0400

    v0.0.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## v0.0.2:
+
+* Scoped Graphite auto-discovery to Chef environment.
+
 ## v0.0.1:
 
 * Initial release.
--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer        "Hector Castro"
 maintainer_email  "hectcastro@gmail.com"
 license           "Apache 2.0"
 description       "Installs and configures collectd."
-version           "0.0.1"
+version           "0.0.2"
 recipe            "collectd", "Installs and configures collectd"
 recipe            "collectd::attribute_driven", "Installs collectd plugins via node attributes"
 
