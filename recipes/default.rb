ab404542f75616913aceaec3c4aa357799281fb7
Adi Hochmann <adi.hochman@gmail.com>
2013-10-22 15:58:46 +0300

    remote_file produced issue with tar extracting, added wget in "install-collects" bash

--- recipes/default.rb
+++ recipes/default.rb
@@ -80,19 +80,14 @@ end
 bash "install-collectd" do
   cwd Chef::Config[:file_cache_path]
   code <<-EOH
+    wget #{node["collectd"]["url"]}
     tar -xzf collectd-#{node["collectd"]["version"]}.tar.gz
     (cd collectd-#{node["collectd"]["version"]} && ./configure --prefix=#{node["collectd"]["dir"]} && make && make install)
   EOH
-  action :nothing
+  action :run
   not_if "#{node["collectd"]["dir"]}/sbin/collectd -h 2>&1 | grep #{node["collectd"]["version"]}"
 end
 
-remote_file "#{Chef::Config[:file_cache_path]}/collectd-#{node["collectd"]["version"]}.tar.gz" do
-  source node["collectd"]["url"]
-  checksum node["collectd"]["checksum"]
-  notifies :run, "bash[install-collectd]", :immediately
-  action :create_if_missing
-end
 
 template "/etc/init.d/collectd" do
   mode "0766"
