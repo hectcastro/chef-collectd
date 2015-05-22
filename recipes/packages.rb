448fdb79253283b6c0a5c02c9639f4c974a21dd7
Paul Dunnavant <paul.dunnavant@vw.com>
2015-05-22 13:30:04 -0700

    Fix syntax error.

--- recipes/packages.rb
+++ recipes/packages.rb
@@ -2,4 +2,4 @@ node["collectd"]["packages"].each do |pkg|
   package pkg
 end
 
-include_recipe 'collectd::_service' only_if { node["collectd"]["packages"].include?("collectd") }
+include_recipe 'collectd::_service' if node["collectd"]["packages"].include?("collectd")
