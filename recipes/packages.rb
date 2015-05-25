8a5cd8cec933914603600f981159484cfbaf1982
Gregoire Seux <g.seux@criteo.com>
2015-05-25 17:08:44 +0200

    Fix packages recipe syntax

--- recipes/packages.rb
+++ recipes/packages.rb
@@ -2,4 +2,6 @@ node["collectd"]["packages"].each do |pkg|
   package pkg
 end
 
-include_recipe 'collectd::_service' only_if { node["collectd"]["packages"].include?("collectd") }
+include_recipe 'collectd::_service' do
+  only_if { node["collectd"]["packages"].include?("collectd") }
+end
