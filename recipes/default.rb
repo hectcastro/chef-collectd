90bfe8af7929420733eafd8f6812d535f0a8009d
Paweł Rein <pawel.rein@smartandco.com>
2016-03-11 16:59:21 +0000

    the installation must preceed plugins configuration obviously

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,9 +4,9 @@ if node["platform_family"] == "rhel" && node["platform_version"].to_i > 5
   end
 end
 
-include_recipe "collectd-ng::plugins" unless node["collectd"]["plugins"].empty?
-
 include_recipe "collectd-ng::install_from_#{node['collectd']['install_method']}"
+
+include_recipe "collectd-ng::plugins" unless node["collectd"]["plugins"].empty?
 
 template "#{node["collectd"]["config_dir"][node['collectd']['install_method']]}/collectd.conf" do
   mode "0644"
