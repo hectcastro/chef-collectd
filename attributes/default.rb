f80fa4c1671be0d7b2a1341b7e6bb1de3cf3e3e9
Sten Spans <sten@blinkenlights.nl>
2016-08-23 13:57:56 +0200

    Make more paths configureable

--- attributes/default.rb
+++ attributes/default.rb
@@ -12,13 +12,17 @@ default["collectd"]["graphite_role"]      = "graphite"
 default["collectd"]["graphite_ipaddress"] = ""
 
 default["collectd"]["source"]["version"]            = "5.4.1"
-default["collectd"]["source"]["dir"]                = "/opt/collectd"
-default["collectd"]["source"]["config_file"]        = ::File.join(node["collectd"]["dir"], "etc", "collectd.conf")
-default["collectd"]["source"]["plugins_conf_dir"]   = ::File.join(node["collectd"]["dir"], "etc", "conf.d")
+default["collectd"]["source"]["base_dir"]           = "/opt/collectd"
+default["collectd"]["source"]["config_file"]        = ::File.join(node["collectd"]["source"]["base_dir"], "etc", "collectd.conf")
+default["collectd"]["source"]["plugins_conf_dir"]   = ::File.join(node["collectd"]["source"]["base_dir"], "etc", "conf.d")
+default["collectd"]["source"]["plugins_dir"]        = ::File.join(node["collectd"]["source"]["base_dir"], "lib", "collectd")
+default["collectd"]["source"]["types_dir"]        = ::File.join(node["collectd"]["source"]["base_dir"], "share", "collectd")
 default["collectd"]["source"]["url"]                = "http://collectd.org/files/collectd-#{node["collectd"]["version"]}.tar.gz"
 default["collectd"]["source"]["checksum"]           = "853680936893df00bfc2be58f61ab9181fecb1cf45fc5cddcb7d25da98855f65"
 
 default["collectd"]["package"]["dir"]               = "/var/lib/collectd"
 default["collectd"]["package"]["config_file"]       = "/etc/collectd.conf"
 default["collectd"]["package"]["plugins_conf_dir"]  = "/etc/collectd.d"
+default["collectd"]["package"]["plugins_dir"]        = "/usr/lib64/collectd"
+default["collectd"]["package"]["types_dir"]         = "/usr/share/collectd/"
 
