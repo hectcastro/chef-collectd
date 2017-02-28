2a1ead570f664bf6e0ac4c2d52aecb9bb3976435
Ian Bamforth <ian.bamforth@tracsis.com>
2017-02-28 10:25:12 +0000

    put the compression type in an attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,8 @@
 default["collectd"]["version"]            = "5.4.1"
 default["collectd"]["dir"]                = "/opt/collectd"
 default["collectd"]["plugins_conf_dir"]    = ::File.join(node["collectd"]["dir"], "etc", "conf.d")
-default["collectd"]["url"]                = "http://collectd.org/files/collectd-#{node["collectd"]["version"]}.tar.gz"
+default["collectd"]["source_compression_type"] = "gz"
+default["collectd"]["url"]                = "http://collectd.org/files/collectd-#{node["collectd"]["version"]}.tar.#{node["collectd"]["source_compression_type"]}"
 default["collectd"]["checksum"]           = "853680936893df00bfc2be58f61ab9181fecb1cf45fc5cddcb7d25da98855f65"
 default["collectd"]["interval"]           = 10
 default["collectd"]["read_threads"]       = 5
--- recipes/default.rb
+++ recipes/default.rb
@@ -66,7 +66,7 @@ if node["collectd"]["plugins"]
   end
 end
 
-remote_file "#{Chef::Config[:file_cache_path]}/collectd-#{node["collectd"]["version"]}.tar.gz" do
+remote_file "#{Chef::Config[:file_cache_path]}/collectd-#{node["collectd"]["version"]}.tar.#{node["collectd"]["source_compression_type"]}" do
   source node["collectd"]["url"]
   checksum node["collectd"]["checksum"]
   action :create_if_missing
@@ -75,7 +75,7 @@ end
 bash "install-collectd" do
   cwd Chef::Config[:file_cache_path]
   code <<-EOH
-    tar -xzf collectd-#{node["collectd"]["version"]}.tar.gz
+    tar -xzf collectd-#{node["collectd"]["version"]}.tar.#{node["collectd"]["source_compression_type"]}
     (cd collectd-#{node["collectd"]["version"]} && ./configure --prefix=#{node["collectd"]["dir"]} #{node["collectd"]["configure_flag"]} && make && make install)
   EOH
   not_if "#{node["collectd"]["dir"]}/sbin/collectd -h 2>&1 | grep #{node["collectd"]["version"]}"
