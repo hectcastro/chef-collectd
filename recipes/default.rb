404b4aef716f7286f70872a79232df209be3ea76
Akos Vandra <axos88@gmail.com>
2020-01-19 04:56:40 +0100

    allow bz archives

--- recipes/default.rb
+++ recipes/default.rb
@@ -75,7 +75,7 @@ end
 bash "install-collectd" do
   cwd Chef::Config[:file_cache_path]
   code <<-EOH
-    tar -xzf collectd-#{node["collectd"]["version"]}.tar.gz
+    tar -xf collectd-#{node["collectd"]["version"]}.tar.gz
     (cd collectd-#{node["collectd"]["version"]} && ./configure --prefix=#{node["collectd"]["dir"]} #{node["collectd"]["configure_flag"]} && make && make install)
   EOH
   not_if "#{node["collectd"]["dir"]}/sbin/collectd -h 2>&1 | grep #{node["collectd"]["version"]}"
